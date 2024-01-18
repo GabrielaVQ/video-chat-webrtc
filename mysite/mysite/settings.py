0bf8c8e522a2fc92a7a386ad1dd332a296b9d5b8
GabyKari <gabrielavq@innovahora.com>
2024-01-17 23:02:02 -0500

    Probando conexión websocket.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -130,3 +130,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ASGI_APPLICATION = "mysite.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
+
